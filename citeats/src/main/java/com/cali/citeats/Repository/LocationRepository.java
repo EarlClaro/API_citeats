e6dd3ee2ea747c6158e6aed3f225d65e49e47ec1
EarlClaro <earlclaro@gmail.com>
2023-11-20 22:37:56 +0800

    Fix error

--- citeats/src/main/java/com/cali/citeats/Repository/LocationRepository.java
+++ citeats/src/main/java/com/cali/citeats/Repository/LocationRepository.java
@@ -10,6 +10,4 @@ import java.util.List;
 
 public interface LocationRepository extends CrudRepository<LocationEntity, Integer> {
 
-    @Query("SELECT l FROM LocationEntity l WHERE l.restaurant.restaurantId = :restaurantId")
-    List<LocationEntity> findByRestaurantId(@Param("restaurantId") int restaurantId);
 }
